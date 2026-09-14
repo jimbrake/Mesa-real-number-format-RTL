ad4cdb86492141d4e92157cbae943eca462fa19c
jimbrake <41881860+jimbrake@users.noreply.github.com>
2026-09-13 21:13:34 -0500

    Add files via upload

---
+++ NSHld_wMesa6-16/t_alu_record.vhd
@@ -0,0 +1,33 @@
+-- Create Date: 07/10/2026
+-- Module Name: alu_record - Behavioral
+-- Target Devices: XCZU3EG-2SFVC784E
+-- Tool Versions: vivado 26.1
+-- Description: 
+-- ALU record type
+-- field sizes from codec_constants.vhd
+-- ALU has an additional exponent bit to catch over/under flow
+-- ALU has additional mantissa bits to support the implicit leading one bit & half bit
+-- ALU exponent is two's complement
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.codec_constants.all;
+
+package t_alu_record is
+    -- Define the record type
+    type t_alu_record is record
+      sign:   std_logic;
+      znan:   std_logic;  				  -- zero or NAR depending on sign bit
+      expon:  signed(expon_sz downto 0);  -- signed for ease of expansion and use plus additional overflow bit
+      frac:   unsigned(frac_sz downto 0); -- include hidden leading bit & half bit
+ end record;
+
+--    -- Optional: Define a constant for easy resetting/initialization
+--    constant alu_rec_init : alu_record := (
+--        sign   => '0',
+--        znan   => '0'
+--        expon  => (others => '0'),
+--        frac   => (others => '0'),
+-- );
+end package t_alu_record;
